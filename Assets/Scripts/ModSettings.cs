5725c089e624823dbbad122c0732cfa2bb337534
ChaoticGraviton <Graviton.Chaotic@gmail.com>
2023-07-02 04:58:31 -0400

    Fixing file overwrite; Local + Repo

--- Assets/Scripts/ModSettings.cs
+++ Assets/Scripts/ModSettings.cs
@@ -1,11 +1,14 @@
 namespace Assets.Scripts
 {
-    using System;
     using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using ModApi.Common;
+    using Assets.Scripts.Craft.Parts.Modifiers.Fuselage;
+    using Assets.Scripts.Design;
+    using Assets.Scripts.Design.Tools.Fuselage;
+    using HarmonyLib;
+    using ModApi.Math;
     using ModApi.Settings.Core;
+    using TMPro;
+    using UnityEngine.UI;
 
     /// <summary>
     /// The settings for the mod.
@@ -32,30 +35,78 @@ namespace Assets.Scripts
         /// The mod settings instance.
         /// </value>
         public static ModSettings Instance => _instance ?? (_instance = Game.Instance.Settings.ModSettings.GetCategory<ModSettings>());
-        public BoolSetting testBool { get; private set; }
+        public BoolSetting DeformationsEnabled { get; private set; }
+        public BoolSetting ClampDistancesEnabled { get; private set; }
+        public BoolSetting WallThicknessEnabled { get; private set; }
 
-        ///// <summary>
-        ///// Gets the TestSetting1 value
-        ///// </summary>
-        ///// <value>
-        ///// The TestSetting1 value.
-        ///// </value>
-        //public NumericSetting<float> TestSetting1 { get; private set; }
 
-        /// <summary>
-        /// Initializes the settings in the category.
-        /// </summary>
         protected override void InitializeSettings()
         {
-           // testBool = this.CreateBool("Enable Stock Sliders")
-           //     .SetDescription("Placeholder Description")
-           //     .SetDefault(false);
+            DeformationsEnabled = this.CreateBool("Advanced Deformation Sliders")
+                .SetDescription("Enables remade sliders for pinch and slant")
+                .SetDefault(true);
+            ClampDistancesEnabled = this.CreateBool("Fuselage Clamp Sliders")
+                .SetDescription("Enables sliders for fuselage clamping")
+                .SetDefault(true);
+            WallThicknessEnabled = this.CreateBool("Fuselage Wall Thickness Sliders")
+                .SetDescription("Enable sliders for fuselage wall thickness")
+                .SetDefault(true);
+        }
+    }
+    [HarmonyPatch(typeof(FuselageShapePanelScript), "RefreshUi")]
+    class RefreshUiPatch
+    {
+        static bool Prefix(FuselageShapePanelScript __instance)
+        {
+            FuselageJoint fuselageJoint = Game.Instance.Designer.GetTool<FuselageShapeTool>().SelectedJoint;
 
+            if (fuselageJoint == null)
+            {
+                return true;
+            }
+            FuselageData fuselageData = fuselageJoint.Fuselages[0].Fuselage.Data;
+            float[] clampDistances = fuselageData.ClampDistances;
+            for (int i = 0; i < 4; i++)
+            {
+                int index = i;
+                if (fuselageData.Script.MarkerBottom == fuselageJoint.Fuselages[0].TargetPoint)
+                {
+                    index += 4;
+                }
+                __instance.xmlLayout.GetElementById<TextMeshProUGUI>("clamp" + (i + 1) + "-value").SetText(Units.GetPercentageString(clampDistances[index]));
+                var slider = __instance.xmlLayout.GetElementById<Slider>("clamp-" + (i + 1));
+                slider.SetValueWithoutNotify(clampDistances[index]);
+            }
+            return true;
+        }
+        static void Postfix(FuselageShapePanelScript __instance)
+        {
+            // Slider enable management
+            var fuselageData = Game.Instance.Designer.GetTool<FuselageShapeTool>();
 
-            //this.TestSetting1 = this.CreateNumeric<float>("Test Setting 1", 1f, 10f, 1f)
-            //    .SetDescription("A test setting that does nothing.")
-            //    .SetDisplayFormatter(x => x.ToString("F1"))
-            //    .SetDefault(2f);
+            if (fuselageData.SelectedFuselage != null)
+            {
+                // For updating sliders sets when selecting the "middle" of a fuselage
+                Traverse.Create(__instance).Field("_sliders").GetValue<List<Slider>>()[5].transform.parent.gameObject.SetActive(value: !ModSettings.Instance.DeformationsEnabled);
+                Traverse.Create(__instance).Field("_sliders").GetValue<List<Slider>>()[6].transform.parent.gameObject.SetActive(value: !ModSettings.Instance.DeformationsEnabled);
+                __instance.xmlLayout.GetElementById<Slider>("pinch-total").transform.parent.gameObject.SetActive(value: ModSettings.Instance.DeformationsEnabled);
+                __instance.xmlLayout.GetElementById<Slider>("pinch-x").transform.parent.gameObject.SetActive(value: ModSettings.Instance.DeformationsEnabled);
+                __instance.xmlLayout.GetElementById<Slider>("pinch-z").transform.parent.gameObject.SetActive(value: ModSettings.Instance.DeformationsEnabled);
+                __instance.xmlLayout.GetElementById<Slider>("iSlant").transform.parent.gameObject.SetActive(value: ModSettings.Instance.DeformationsEnabled);
+                __instance.xmlLayout.GetElementById("thickness-top").transform.parent.gameObject.SetActive(value: ModSettings.Instance.WallThicknessEnabled);
+                __instance.xmlLayout.GetElementById("thickness-bottom").transform.parent.gameObject.SetActive(value: ModSettings.Instance.WallThicknessEnabled);
+                __instance.xmlLayout.GetElementById("thickness-disabled").transform.parent.gameObject.SetActive(value: !ModSettings.Instance.WallThicknessEnabled);
+            }
+            else if (fuselageData.SelectedJoint.Transform.position == fuselageData.SelectedJoint.Fuselages[0].Fuselage.Data.Script.MarkerBottom.position && fuselageData.SelectedJoint.Transform.position != null) ;
+            {
+                // For updating sliders sets when selecting the "top/bottom" of a fuselage
+                __instance.xmlLayout.GetElementById<Slider>("clamp-1").transform.parent.gameObject.SetActive(value: ModSettings.Instance.ClampDistancesEnabled);
+                __instance.xmlLayout.GetElementById<Slider>("clamp-2").transform.parent.gameObject.SetActive(value: ModSettings.Instance.ClampDistancesEnabled);
+                __instance.xmlLayout.GetElementById<Slider>("clamp-3").transform.parent.gameObject.SetActive(value: ModSettings.Instance.ClampDistancesEnabled);
+                __instance.xmlLayout.GetElementById<Slider>("clamp-4").transform.parent.gameObject.SetActive(value: ModSettings.Instance.ClampDistancesEnabled);
+                __instance.xmlLayout.GetElementById("default-clamp").transform.parent.gameObject.SetActive(value: ModSettings.Instance.ClampDistancesEnabled);
+                __instance.xmlLayout.GetElementById("clamp-disabled").transform.parent.gameObject.SetActive(value: !ModSettings.Instance.ClampDistancesEnabled);
+            }
         }
     }
 }
